5578f96476eebb622e1e9342f5d38d101683c7e6
apriya3 <apriya3@buffalo.edu>
2026-02-25 01:06:57 -0800

    added google reviews link

--- src/components/section-blog/index.jsx
+++ src/components/section-blog/index.jsx
@@ -16,6 +16,11 @@ const SectionBlog = ({ posts }) => {
           internal
         />
       ))}
+      <SummaryItem
+        name="Google Maps Reviews"
+        description="My reviews on Google Maps"
+        link="https://google.com/maps/contrib/101112107729949975275/reviews/@47.6176503,-122.355194,13z/data=!4m3!8m2!3m1!1e1?authuser=1&entry=ttu&g_ep=EgoyMDI2MDIyMi4wIKXMDSoASAFQAw%3D%3D"
+      />
       {posts.length >= 5 && (
         <Link className="text-gray-500 text-sm hover:text-black" to="/blog">
           View all posts &rarr;
